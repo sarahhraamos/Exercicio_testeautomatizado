7b9bc933932a9b1452c1fe30538a54c139e37e57
sarahhraamos <sarah17ramos@gmail.com>
2021-03-23 21:01:50 -0300

    first commit

---
+++ src/test/java/io/cucumber/danilo/ExercicioSteps.java
@@ -0,0 +1,45 @@
+package io.cucumber.danilo;
+
+import static org.junit.Assert.assertEquals;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.cucumber.java.pt.Entao;
+import io.cucumber.java.pt.Quando;
+
+public class ExercicioSteps {
+
+        public ExercicioSteps() {
+            System.setProperty("webdriver.chrome.driver", "driver/chromedriver.exe");
+            browser = new ChromeDriver();
+        }
+
+        public WebDriver browser;
+
+        @Quando("eu acesso o site do tornese um programador")
+            public void que_que_estou_no_site_do_tornese_um_programador() {
+            browser.get("https://www.torneseumprogramador.com.br/");
+        }
+        
+        @Quando("digito no campo busca {string}")
+            public void digito_no_campo_busca(String string) {
+            WebElement input = browser.findElement( By.cssSelector("input[name='q']") );
+            input.sendKeys(string);
+        }
+        
+        @Quando("clico no botao pesquisar")
+            public void clico_no_botao_pesquisar() {
+            WebElement input = browser.findElement( By.cssSelector("button[class='btn btn-outline-dark btn-pesquisa']") );
+            input.click();
+        }
+
+        @Entao("devo acessar a pagina com resultados de busca")
+            public void devo_acessar_a_pagina_com_resultados_de_busca() {
+            assertEquals(true , browser.findElements( By.cssSelector(".div-card-aulas") ).size() > 0);
+            browser.quit();
+        }
+
+}
---
+++ src/test/resources/io/cucumber/danilo/teste.feature
@@ -0,0 +1,18 @@
+# language: pt
+Funcionalidade: Pesquisa em site
+  Entrar no site torneseumprogramador e pesquisar 
+   
+  Cenario: Pesquisando tdd
+	  Quando eu acesso o site do tornese um programador 
+	  E digito no campo busca "tdd"
+	  E clico no botao pesquisar
+	  Entao devo acessar a pagina com resultados de busca
+	  
+	Cenario: Pesquisando bdd
+	  Quando eu acesso o site do tornese um programador 
+	  E digito no campo busca "bdd"
+	  E clico no botao pesquisar
+	  Entao devo acessar a pagina com resultados de busca
+  
+
+   
